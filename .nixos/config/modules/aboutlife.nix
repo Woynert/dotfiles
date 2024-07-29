cd866b0fc8eb1dac656b46e2d2d7c717e677c215
woynert <woynertgamer@gmail.com>
2024-07-29 12:47:48 -0500

    chore: bump aboutlife version

--- .nixos/config/modules/aboutlife.nix
+++ .nixos/config/modules/aboutlife.nix
@@ -5,10 +5,10 @@ let
   # If the same sha256 is left nix won't update the tarball
   # TODO: come up with something better
 
-  rev = "26288e7ffa08a64c42a0372e877be45f09d4808c"; # or "master"
+  rev = "3338c35d70c07ed4e85847e55aa060017a1c2633"; # or "master"
   aboutlife = fetchTarball {
     url = "https://github.com/woynert/aboutlife/archive/${rev}.tar.gz";
-    sha256 = "0sfk666rxdrardyrwzsq5a86lyrk1q6r2kbppld03wla6r5m1pcj";
+    sha256 = "0sx1gnngxihv1f3vzr1q9icj8hrq7gmq7bklkrp2hcac4x5wwj0x";
   };
 
 in
